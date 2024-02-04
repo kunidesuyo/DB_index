d4c31b8fb8f24e7760edba6fbd10c7b23099b8c9
kuni <kunidesuyo1234@gmail.com>
2024-02-04 21:07:24 +0900

    change time

--- queries/template/time.sh
+++ queries/template/time.sh
@@ -1,1 +1,6 @@
+echo "no index"
 time(cat /queries/template/select.sql | mysql -uuser -ppassword db > /dev/null)
+echo -e "\n"
+
+echo "using index"
+time(cat /queries/template/select_using_index.sql | mysql -uuser -ppassword db > /dev/null)
