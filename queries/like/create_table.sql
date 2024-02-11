6a15d4b57eed3d4f26045ca72d88119a1ae64844
kuni <kunidesuyo1234@gmail.com>
2024-02-11 23:39:37 +0900

    update like

--- queries/like/create_table.sql
+++ queries/like/create_table.sql
@@ -1,16 +1,5 @@
-create table generator_char(str varchar(10));
-insert into generator_char values('A');
-insert into generator_char values('B');
-insert into generator_char values('C');
-insert into generator_char values('D');
-insert into generator_char values('E');
-insert into generator_char values('F');
-insert into generator_char values('G');
-insert into generator_char values('H');
-insert into generator_char values('I');
-insert into generator_char values('');
-
 CREATE TABLE like_table (
-   id int auto_increment primary key,
-   str VARCHAR(10) NOT NULL
-   );
+   id INT NOT NULL,
+   str VARCHAR(255) NOT NULL,
+   PRIMARY KEY (id)
+);
